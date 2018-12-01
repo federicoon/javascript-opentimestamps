d04fdd580126b47b2a2711a0b1ec824c80534a2e
federicoon <federicoon@users.noreply.github.com>
2018-12-01 21:27:50 +0100

    Update blockstream.js
    Added code to query Blockstream API

--- src/blockstream.js
+++ src/blockstream.js
@@ -1,1 +1,125 @@
+'use strict'
 
+/**
+ * Blockstream module.
+ * @module Blockstream
+ * @author federicoon
+ * @license LPGL3
+ */
+
+const requestPromise = require('request-promise')
+const Promise = require('promise')
+const Utils = require('./utils.js')
+
+/** Class used to query Blockstream API */
+class Blockstream {
+  /**
+   * Create a RemoteCalendar.
+   * @param {int} timeout - timeout (in seconds) used for calls to Blockstream server
+   */
+  constructor (url, timeout) {
+    this.urlBlockindex = url + '/block-height'
+    this.urlBlock = url + '/block'
+    this.timeout = timeout * 1000
+  }
+
+  /**
+   * This callback is called when the result is loaded.
+   *
+   * @callback resolve
+   * @param {Timestamp} timestamp - The timestamp of the Calendar response.
+   */
+
+  /**
+   * This callback is called when the result fails to load.
+   *
+   * @callback reject
+   * @param {Error} error - The error that occurred while loading the result.
+   */
+
+  /**
+   * Retrieve the block hash from the block height.
+   * @param {string} height - Height of the block.
+   * @returns {Promise} A promise that returns {@link resolve} if resolved
+   * and {@link reject} if rejected.
+   */
+  blockhash (height) {
+    const options = {
+      url: this.urlBlockindex + '/' + height,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'javascript-opentimestamps',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      json: true,
+      timeout: this.timeout
+    }
+
+    return new Promise((resolve, reject) => {
+      requestPromise(options)
+        .then(body => {
+          // console.log('body ', body);
+          if (body.size === 0) {
+            console.error('Blockstream response error body ')
+            reject(new Error('Blockstream response error body '))
+            return
+          }
+
+          resolve(body.id)
+        })
+        .catch(err => {
+          console.error('Blockstream response error: ' + err.toString().substr(0, 100))
+          reject(err)
+        })
+    })
+  }
+
+  /**
+   * Retrieve the block information from the block hash.
+   * @param {string} height - Height of the block.
+   * @returns {Promise} A promise that returns {@link resolve} if resolved
+   * and {@link reject} if rejected.
+   */
+  block (hash) {
+    const options = {
+      url: this.urlBlock + '/' + hash,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'javascript-opentimestamps',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      json: true,
+      timeout: this.timeout
+    }
+
+    return new Promise((resolve, reject) => {
+      requestPromise(options)
+        .then(body => {
+          // console.log('body ', body);
+          if (!body) {
+            console.error('Blockstream response error body ')
+            return reject(new Error('Blockstream response error body '))
+          }
+          if (!body.merkleroot || !body.time) {
+            return reject(new Error('Blockstream response error body '))
+          }
+          resolve({merkleroot: body.merkle_root, time: body.timestamp})
+        })
+        .catch(err => {
+          console.error('Blockstream response error: ' + err.toString().substr(0, 100))
+          reject(err)
+        })
+    })
+  }
+}
+
+const publicBlockstreamUrls = {
+  mainnet: 'https://blockstream.info/api',
+  testnet: 'https://blockstream.info/testnet/api'
+}
+
+module.exports = {
+  Blockstream
+}
