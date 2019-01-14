b413d72848a0a11dbef203fa3839eeac50634d9d
federicoon <gi4c0ph@gmail.com>
2019-01-14 13:09:14 +0100

    commented out User-Agent headers on HTTP requests to block explorers

--- src/chain-explorer.js
+++ src/chain-explorer.js
@@ -36,7 +36,10 @@ class ChainExplorer {
       method: 'GET',
       headers: {
         Accept: 'application/json',
-        'User-Agent': 'javascript-opentimestamps',
+        
+        // will fail on some browsers (e.g. Firefox) as CORS policy triggers pre-flight request
+        // see https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Preflighted_requests
+        //'User-Agent': 'javascript-opentimestamps',
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       json: true,
@@ -67,7 +70,10 @@ class ChainExplorer {
       method: 'GET',
       headers: {
         Accept: 'application/json',
-        'User-Agent': 'javascript-opentimestamps',
+        
+        // will fail on some browsers (e.g. Firefox) as CORS policy triggers pre-flight request
+        // see https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Preflighted_requests
+        //'User-Agent': 'javascript-opentimestamps',
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       json: true,
